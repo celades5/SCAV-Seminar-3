b0ff0b6e7702f00ecb6e3fa2dd06d0c5456d09dc
Xavier Celades <xcelades@gmail.com>
2020-12-02 15:27:21 +0100

    Final python code for exercise 2

---
+++ S3_ex2.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+
+
+def ex2():
+
+    # Mosaico
+    os.system('ffmpeg \
+            -i 480p_VP8.webm \
+            -i 480p_VP9.webm \
+            -i 480p_h265.mp4 \
+            -i 480p_AV1.mkv \
+            -filter_complex " \
+            [0:v] setpts=PTS-STARTPTS, scale=qvga [a0]; \
+            [1:v] setpts=PTS-STARTPTS, scale=qvga [a1]; \
+            [2:v] setpts=PTS-STARTPTS, scale=qvga [a2]; \
+            [3:v] setpts=PTS-STARTPTS, scale=qvga [a3]; \
+            [a0][a1][a2][a3]xstack=inputs=4:layout=0_0|0_h0|w0_0|w0_h0[out] \
+            " \
+            -map "[out]" \
+            -c:v libx264 -t 30 -f matroska 4in1.mp4')
+
+
+if __name__ == "__main__":
+
+    ex2()
